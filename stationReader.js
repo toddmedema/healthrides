991142d351946116d221222d4512cd2506b5a9ab
Rebecca Dilla <rid@Rebeccas-MacBook-Pro.local>
2017-04-05 19:27:40 -0400

    init: Rebecca

---
+++ stationReader.js
@@ -0,0 +1,385 @@
+
+
+var stations = {
+              "1000": {
+                "Name": "Liberty Ave & Stanwix St",
+                "# of Racks": 16,
+                "Latitude": 40.441326,
+                "Longitude": -80.004679
+              },
+              "1001": {
+                "Name": "Forbes Ave & Market Square",
+                "# of Racks": 19,
+                "Latitude": 40.440877,
+                "Longitude": -80.00308
+              },
+              "1002":{
+                "Name": "Third Ave & Wood St",
+                "# of Racks": 15,
+                "Latitude": 40.43903,
+                "Longitude": -80.00186
+              },
+              "1003":{
+                "Name": "First Ave & Smithfield St (Art Institute)",
+                "# of Racks": 15,
+                "Latitude": 40.4372,
+                "Longitude": -80.000375
+              },
+              "1004":{
+                "Name": "First Ave & B St (T Station)",
+                "# of Racks": 15,
+                "Latitude": 40.435887,
+                "Longitude": -79.997102
+              },
+              "1005":{
+                "Name": "Forbes Ave & Grant St",
+                "# of Racks": 19,
+                "Latitude": 40.438882,
+                "Longitude": -79.997592
+              },
+              "1006":{
+                "Name": "Ross St & Sixth Ave (Steel Plaza T Station)",
+                "# of Racks": 21,
+                "Latitude": 40.440193,
+                "Longitude": -79.995084
+              },
+              "1007":{
+                "Name": "Stevenson St & Forbes Ave",
+                "# of Racks": 19,
+                "Latitude": 40.437643,
+                "Longitude": -79.986695
+              },
+              "1008":{
+                "Name": "Centre Ave & PPG Paints Arena",
+                "# of Racks": 35,
+                "Latitude": 40.440368,
+                "Longitude": -79.988636
+              },
+              "1009":{
+                "Name": "12th St & Penn Ave",
+                "# of Racks": 19,
+                "Latitude": 40.445844,
+                "Longitude": -79.99238
+              },
+              "1010":{
+                "Name": "10th St & Penn Ave (David L. Lawrence Convention Center)",
+                "# of Racks": 15,
+                "Latitude": 40.444665,
+                "Longitude": -79.995798
+              },
+              "1011":{
+                "Name": "Fort Duquesne Blvd & 7th",
+                "# of Racks": 15,
+                "Latitude": 40.444777,
+                "Longitude": -80.000831
+              },
+              "1012":{
+                "Name": "North Shore Trail & Ft Duquesne Bridge",
+                "# of Racks": 35,
+                "Latitude": 40.445834,
+                "Longitude": -80.008882
+              },
+              "1013":{
+                "Name": "Isabella St & Federal St (PNC Park)",
+                "# of Racks": 15,
+                "Latitude": 40.447166,
+                "Longitude": -80.003566
+              },
+              "1014":{
+                "Name": "Ridge Ave & Brighton Rd (CCAC)",
+                "# of Racks": 19,
+                "Latitude": 40.450595,
+                "Longitude": -80.013204
+              },
+              "1015":{
+                "Name": "Federal St & E North Ave",
+                "# of Racks": 12,
+                "Latitude": 40.45509087,
+                "Longitude": -80.0063467
+              },
+              "1016":{
+                "Name": "17th St & Penn Ave",
+                "# of Racks": 19,
+                "Latitude": 40.449631,
+                "Longitude": -79.985893
+              },
+              "1017":{
+                "Name": "21st St & Penn Ave",
+                "# of Racks": 18,
+                "Latitude": 40.451742,
+                "Longitude": -79.983217
+              },
+              "1018":{
+                "Name": "37th St & Butler St",
+                "# of Racks": 21,
+                "Latitude": 40.466103,
+                "Longitude": -79.964628
+              },
+              "1019":{
+                "Name": "42nd St & Butler St",
+                "# of Racks": 17,
+                "Latitude": 40.47021248,
+                "Longitude": -79.96066332
+              },
+              "1020":{
+                "Name": "42nd St & Penn Ave (Children's Hospital)",
+                "# of Racks": 15,
+                "Latitude": 40.465893,
+                "Longitude": -79.954417
+              },
+              "1021":{
+                "Name": "Taylor St & Liberty Ave",
+                "# of Racks": 19,
+                "Latitude": 40.462769,
+                "Longitude": -79.950867
+              },
+              "1022":{
+                "Name": "Liberty Ave & S Millvale (West Penn Hospital)",
+                "# of Racks": 15,
+                "Latitude": 40.459812,
+                "Longitude": -79.945548
+              },
+              "1023":{
+                "Name": "Liberty Ave & Baum Blvd",
+                "# of Racks": 19,
+                "Latitude": 40.456505,
+                "Longitude": -79.939362
+              },
+              "1024":{
+                "Name": "S Negley Ave & Baum Blvd",
+                "# of Racks": 17,
+                "Latitude": 40.458714,
+                "Longitude": -79.933483
+              },
+              "1025":{
+                "Name": "Penn Ave & N Fairmount St",
+                "# of Racks": 15,
+                "Latitude": 40.464443,
+                "Longitude": -79.933188
+              },
+              "1026":{
+                "Name": "Penn Ave & S Whitfield St",
+                "# of Racks": 13,
+                "Latitude": 40.461603,
+                "Longitude": -79.925624
+              },
+              "1027":{
+                "Name": "Shady Ave & Ellsworth Ave",
+                "# of Racks": 19,
+                "Latitude": 40.458972,
+                "Longitude": -79.922023
+              },
+              "1028":{
+                "Name": "Penn Ave & Putnam St (Bakery Square)",
+                "# of Racks": 19,
+                "Latitude": 40.455821,
+                "Longitude": -79.915248
+              },
+              "1029":{
+                "Name": "Alder St & S Higland Ave",
+                "# of Racks": 19,
+                "Latitude": 40.457415,
+                "Longitude": -79.92492
+              },
+              "1030":{
+                "Name": "S Euclid Ave & Centre Ave",
+                "# of Racks": 12,
+                "Latitude": 40.458933,
+                "Longitude": -79.929123
+              },
+               "1031":{
+                "Name": "Maryland Ave & Ellsworth Ave",
+                "# of Racks": 19,
+                "Latitude": 40.45628,
+                "Longitude": -79.930962
+              },
+               "1032":{
+                "Name": "Walnut St & College St",
+                "# of Racks": 14,
+                "Latitude": 40.453382,
+                "Longitude": -79.92731
+              },
+               "1033":{
+                "Name": "Ivy St & Walnut St",
+                "# of Racks": 19,
+                "Latitude": 40.45177,
+                "Longitude": -79.932324
+              },
+               "1034":{
+                "Name": "Ellsworth Ave & N Neville St",
+                "# of Racks": 15,
+                "Latitude": 40.448419,
+                "Longitude": -79.947401
+              },
+               "1035":{
+                "Name": "Fifth Ave & S Dithridge St",
+                "# of Racks": 19,
+                "Latitude": 40.446744,
+                "Longitude": -79.950881
+              },
+               "1036":{
+                "Name": "Schenley Dr at Schenley Plaza (Carnegie Library Main)",
+                "# of Racks": 19,
+                "Latitude": 40.442398,
+                "Longitude": -79.951479
+              },
+               "1037":{
+                "Name": "Frew St & Schenley Dr",
+                "# of Racks": 20,
+                "Latitude": 40.441032,
+                "Longitude": -79.948042
+              },
+               "1038":{
+                "Name": "Boulevard of the Allies & Parkview Ave",
+                "# of Racks": 19,
+                "Latitude": 40.434338,
+                "Longitude": -79.951877
+              },
+               "1039":{
+                "Name": "Atwood St & Bates",
+                "# of Racks": 19,
+                "Latitude": 40.437987,
+                "Longitude": -79.95367
+              },
+               "1040":{
+                "Name": "Bigelow Blvd & Fifth Ave",
+                "# of Racks": 21,
+                "Latitude": 40.444708,
+                "Longitude": -79.955025
+              },
+              "1041":{
+                "Name": "Fifth Ave & S Bouquet St",
+                "# of Racks": 19,
+                "Latitude": 40.442325,
+                "Longitude": -79.957604
+              },
+              "1042":{
+                "Name": "Centre Ave & Kirkpatrick St",
+                "# of Racks": 12,
+                "Latitude": 40.445236,
+                "Longitude": -79.976911
+              },
+              "1043":{
+                "Name": "Coltart Ave & Forbes Ave",
+                "# of Racks": 15,
+                "Latitude": 40.438876,
+                "Longitude": -79.960179
+              },
+              "1044":{
+                "Name": "Zulema St & Coltart Ave",
+                "# of Racks": 19,
+                "Latitude": 40.435986,
+                "Longitude": -79.956942
+              },
+              "1045":{
+                "Name": "S 27th St & Sidney St. (Southside Works)",
+                "# of Racks": 19,
+                "Latitude": 40.427899,
+                "Longitude": -79.966112
+              },
+              "1046":{
+                "Name": "S 25th St & E Carson St",
+                "# of Racks": 19,
+                "Latitude": 40.42802,
+                "Longitude": -79.969799
+              },
+              "1047":{
+                "Name": "S 22nd St & E Carson St",
+                "# of Racks": 19,
+                "Latitude": 40.428576,
+                "Longitude": -79.974559
+              },
+              "1048":{
+                "Name": "S 18th St & Sidney St",
+                "# of Racks": 16,
+                "Latitude": 40.429338,
+                "Longitude": -79.980684
+              },
+              "1049":{
+                "Name": "S 12th St & E Carson St",
+                "# of Racks": 19,
+                "Latitude": 40.428661,
+                "Longitude": -79.986358
+              }
+            };
+
+var colors = ["red", "blue", "yellow"];
+
+
+function drawStations(){
+            for(var i in stations){
+            	randCol = Math.floor(Math.random() * 3);
+                var station = stations[i];
+                station.circle = L.circle([station.Latitude, station.Longitude],{
+                fillColor: colors[randCol],
+                color: colors[randCol],
+                fillOpacity: 0.5,
+                radius: (Math.random() * 50 + 50), 
+                }).bindPopup(station.Name)
+                .addTo(mymap);
+
+            }  
+
+	}	
+
+	function updateStations(){
+            for(var i in stations){
+            	randCol = Math.floor(Math.random() * 3);
+                var station = stations[i];
+                station.circle.setStyle({fillColor: colors[randCol], color: colors[randCol]});
+                station.circle.setRadius(Math.random() * 50 + 50) 
+            }  
+
+	}	
+
+
+
+var running = false;
+		var timer;
+		
+		$("button").on("click", function() {
+		
+			var duration = 1000,
+				maxstep = 100,
+				minstep = 1;
+			
+			if (running == true) {
+			
+				$("button").html("Play");
+				running = false;
+				clearInterval(timer);
+				
+			} 
+
+			else if (running == false) {
+			
+				$("button").html("Pause");
+				
+				sliderValue = $("#slider").val();
+				
+				timer = setInterval( function(){
+						if (sliderValue < maxstep){
+							sliderValue++;
+							$("#slider").val(sliderValue);
+							$('#range').html(sliderValue);
+						}
+						$("#slider").val(sliderValue);
+						updateStations();
+					
+				}, duration);
+				running = true;
+				
+				
+			}
+
+		});
+	
+		$("#slider").on("change", function(){
+			updateStations();
+			$("#range").html($("#slider").val());
+			clearInterval(timer);
+			$("button").html("Play");
+		});
+
+
+
