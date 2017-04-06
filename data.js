76ba8f645119cec373b9c241d5cae695a65c8b90
Todd Medema <ToddMedema@gmail.com>
2017-04-06 01:17:51 -0400

    working

--- data.js
+++ data.js
@@ -2,10 +2,12 @@ function downloadData(callback) {
   download('data/HealthyRideRentals' + year + '.csv', (err, rentals) => {
     if (!err) {
       data.rides = processRentals(rentals);
+    } else {
+      return callback(err);
     }
     download('data/HealthyRideStations' + year + '.csv', (err, stations) => {
       if (!err) {
-        data.stations = processStations(stations);
+        data.stations = processStations(data.rides, stations);
       }
       callback(err);
     });
@@ -31,20 +33,50 @@ function processRentals(csv) {
   let data = [];
   for (let i = 0, l = csv.length; i < l; i++) {
     const ride = csv[i];
-    ride.startRounded = moment(ride.Starttime).startOf('hour').toString();
-    ride.startDate = moment(ride.Starttime).startOf('day').format('YYYY-MM-DD');
-    ride.stopRounded = moment(ride.Stoptime).startOf('hour').toString();
+    // TODO hour of day and day of week strings
+    // ride.startHour = moment(ride.Starttime).startOf('hour').toString();
+    // ride.startHourFormatted = ride.startHour();
+    ride.startDate = moment(ride.Starttime).startOf('day');
+    ride.startDateFormatted = ride.startDate.format('YYYY-MM-DD');
+    // ride.stopRounded = moment(ride.Stoptime).startOf('hour').toString();
     data.push(ride);
   }
   return data;
 }
 
-function processStations(csv) {
+function processStations(rides, csv) {
   csv = d3.csvParse(csv);
   let data = {};
   for (let i = 0, l = csv.length; i < l; i++) {
     const station = csv[i];
     station.id = station['Station #'];
+    station.name = station['Station Name'];
+    // calculate net inflow/outflow per day
+    // inflow + outflow not guaranteed to have a value for every day
+    station.dailyOutflow = rides.reduce((days, ride) => {
+      if (ride['From station id'] === station.id) {
+        if (days[ride.startDateFormatted]) {
+          days[ride.startDateFormatted] += 1;
+        } else {
+          days[ride.startDateFormatted] = 1;
+        }
+      }
+      return days;
+    }, {});
+    station.dailyInflow = rides.reduce((days, ride) => {
+      if (ride['To station id'] === station.id) {
+        if (days[ride.startDateFormatted]) {
+          days[ride.startDateFormatted] += 1;
+        } else {
+          days[ride.startDateFormatted] = 1;
+        }
+      }
+      return days;
+    }, {});
+    station.dailyNet = daysArray.reduce((days, day) => {
+      days[day] = (station.dailyInflow[day] || 0) - (station.dailyOutflow[day] || 0);
+      return days;
+    }, {});
     data[station.id] = station;
   }
   return data;
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// TODOs
+// add time of day filter slider
+// add checkboxes for day of week
+// stylize page + provide help info
+// host on PittsburghBikeWorks.com
+
+// BONUS ideas
+// Integrate weather data for exploration
+// Integrate moves data for exploration
+// update circle popup text with new data on refreshes
+// Change axis: per day of year vs per hour of week
+// Allow for animated playthrough
+  // Perhaps separate calculation from visual updating? Animation data is pre-calculable
+  // Plus, could then cache previous calculations
+
+
 const year = 2016; // TODO combine all years' data, and let users filter / view all of it
 const data = {
   moves: [], // [objects] rental data - interpolated moves
@@ -6,12 +22,17 @@ const data = {
   weather: {},// {ts (hourly): {weather}}
 };
 const filters = {
-  minTime: '2016-01-01',
-  maxTime: '2017-01-01',
-  startTime: '2016-01-01',
-  endTime: '2017-01-01',
-}
-const colors = ["red", "blue", "yellow"];
+  minTime: moment('2016-01-01'),
+  maxTime: moment('2017-01-01'),
+  startTime: moment('2016-01-01'),
+  endTime: moment('2017-01-01'),
+};
+const daysArray = calculateDaysArray(filters.minTime, filters.maxTime);
+const markers = {
+  '2016-09-24': 'Pirates game',
+};
+const CIRCLE_RADIUS_MAX = 200;
+const CIRCLE_RADIUS_MIN = 50;
 
 let windowWidth = 0;
 let windowHeight = 0;
@@ -39,12 +60,22 @@ downloadData((err) => {
 
 // HELPERS
 
+function calculateDaysArray(startMoment, endMoment) {
+  let day = startMoment;
+  let days = [];
+  while (moment(day).isSameOrBefore(endMoment)) {
+    days.push(day.format('YYYY-MM-DD'));
+    day = day.add(1, 'day');
+  }
+  return days;
+}
+
 function createStations() {
   for (let i in data.stations) {
   	randCol = Math.floor(Math.random() * 3);
     const station = data.stations[i];
     station.circle = L.circle([station.Latitude, station.Longitude])
-      .bindPopup(station.Name)
+      .bindPopup(station.name)
       .addTo(mymap);
   }
   updateStations();
@@ -52,45 +83,109 @@ function createStations() {
 
 function updateSizes() {
   windowWidth = $(window).width();
-  windowHeight = $(window).height();
+  windowHeight = $(window).height() - $("#topbar").height();
   $("#mapid")
     .width(windowWidth)
     .height(windowHeight * 2/3);
+  $("#timeseries")
+    .width(windowWidth)
+    .height(windowHeight * 1/3);
   updateTimeseries();
 }
 
+// http://leafletjs.com/reference-1.0.3.html
 function updateStations() {
-  for (let i in data.stations) {
-  	randCol = Math.floor(Math.random() * 3);
-    const station = data.stations[i];
+  const days = daysArray.filter((day) => {
+      return (moment(day).isBetween(filters.startTime, filters.endTime));
+    });
+  const stationNets = Object.keys(data.stations)
+    .reduce((stationNets, id) => {
+      const station = data.stations[id];
+      stationNets[station.id] = days.reduce((sum, day) => {
+        return sum + (station.dailyNet[day] || 0);
+      }, 0)
+      return stationNets;
+    }, {});
+  const netArr = Object.values(stationNets);
+  const netMin = Math.min(0, ...netArr);
+  const netMax = Math.max(0, ...netArr);
+  const netAbsMax = Math.max(Math.abs(netMin), Math.abs(netMax));
+
+  const stationAbs = Object.keys(data.stations)
+    .reduce((stationAbs, id) => {
+      const station = data.stations[id];
+      stationAbs[station.id] = days.reduce((sum, day) => {
+        return sum + (station.dailyInflow[day] || 0) + (station.dailyOutflow[day] || 0);
+      }, 0)
+      return stationAbs;
+    }, {});
+  const absArr = Object.values(stationAbs);
+  const absMin = Math.min(...absArr);
+  const absMax = Math.max(...absArr);
+
+  for (let id in data.stations) {
+    const station = data.stations[id];
+    // calculate net color: positive (blue) vs negative (red) vs grey when near 0
+    const net = stationNets[id];
+    const diff = 200 * Math.abs(net) / netAbsMax; // against abs max to highlight relative sizes better
+    let colorR = 200;
+    let colorG = 200;
+    let colorB = 200;
+    if (net > 0) { // positive: increase B, decrease RG
+      colorB += diff;
+      colorR -= diff;
+      colorG -= diff;
+    } else { // negative: increase R, decreate GB
+      colorR += diff;
+      colorG -= diff;
+      colorB -= diff;
+    }
+    const color = `rgb(${Math.round(Math.min(255, colorR))},${Math.round(Math.min(255, colorG))},${Math.round(Math.min(255, colorB))})`;
+console.log(diff, color);
     station.circle
       .setStyle({
-        fillColor: colors[randCol],
-        color: colors[randCol],
-        fillOpacity: 0.1,
+        fillColor: color,
+        color: color,
+        fillOpacity: 0.8,
       })
-      .setRadius(Math.random() * 50 + 50)
+      .setRadius(CIRCLE_RADIUS_MIN + (CIRCLE_RADIUS_MAX - CIRCLE_RADIUS_MIN) * (stationAbs[id] - absMin) / (absMax - absMin));
   }
 }
 
 function updateTimeseries() {
   const ridesPerDay = {};
+  let days = [];
+  // TODO use reducer
   for (let i = 0, l = data.rides.length; i < l; i++) {
     const ride = data.rides[i];
-    ridesPerDay[ride.startDate] = ridesPerDay[ride.startDate] || 0;
-    ridesPerDay[ride.startDate] += 1;
+    if (moment(ride.startDate).isBetween(filters.startTime, filters.endTime)) {
+      if (ridesPerDay[ride.startDateFormatted] == null) {
+        days.push(ride.startDateFormatted);
+        ridesPerDay[ride.startDateFormatted] = 0;
+      }
+      ridesPerDay[ride.startDateFormatted] += 1;
+    }
   }
-console.log(ridesPerDay);
+  days = days.sort();
+  const timeseries = days.map((day) => {
+    return {date: new Date(day), value: ridesPerDay[day]}
+  });
+  // TODO expand list of markers, only show markers for top (3?) data points in filter range
+  const markersFormatted = Object.keys(markers).map((date) => {
+    return {date: new Date(date), label: markers[date]};
+  });
+
+  // https://github.com/mozilla/metrics-graphics/wiki/List-of-Options
   MG.data_graphic({
-    title: "Downloads",
-    description: "This graphic shows a time-series of downloads.",
-    data: [{'date':new Date('2014-11-01'),'value':12},
-           {'date':new Date('2014-11-02'),'value':18}],
+    title: 'Rides Per Day',
+    data: timeseries,
+    buffer: 0,
     width: windowWidth,
     height: windowHeight/3,
     target: '#timeseries',
     x_accessor: 'date',
     y_accessor: 'value',
+    markers: markersFormatted,
   });
 }
 
@@ -98,33 +193,55 @@ console.log(ridesPerDay);
 
 $(window).resize(updateSizes);
 
-$("button").on("click", function() {
-	var duration = 1000,
-		maxstep = 100,
-		minstep = 1;
-	if (running == true) {
-		$("button").html("Play");
-		running = false;
-		clearInterval(timer);
-	} else if (running == false) {
-		$("button").html("Pause");
-		sliderValue = $("#slider").val();
-		timer = setInterval( function(){
-			if (sliderValue < maxstep){
-				sliderValue++;
-				$("#slider").val(sliderValue);
-				$('#range').html(sliderValue);
-			}
-			$("#slider").val(sliderValue);
-			updateStations();
-		}, duration);
-		running = true;
-	}
+// http://nitinhayaran.github.io/jRange/demo/
+$('.range-slider').jRange({
+  from: 0,
+  to: 365,
+  step: 1,
+  showScale: false,
+  format: (value) => { return moment(year+'-01-01').add(value, 'days').format('MMM D'); },
+  width: windowWidth - 40,
+  isRange : true,
+  ondragend: (value) => {
+    value = value.split(',');
+    filters.startTime = moment(year+'-01-01').add(value[0], 'days');
+    filters.endTime = moment(year+'-01-01').add(value[1], 'days');
+    updateTimeseries();
+    updateStations();
+  },
 });
 
-$("#slider").on("change", function(){
-	updateStations();
-	$("#range").html($("#slider").val());
-	clearInterval(timer);
-	$("button").html("Play");
-});
+
+
+
+
+// $("button").on("click", function() {
+// 	var duration = 1000,
+// 		maxstep = 100,
+// 		minstep = 1;
+// 	if (running == true) {
+// 		$("button").html("Play");
+// 		running = false;
+// 		clearInterval(timer);
+// 	} else if (running == false) {
+// 		$("button").html("Pause");
+// 		sliderValue = $("#slider").val();
+// 		timer = setInterval( function(){
+// 			if (sliderValue < maxstep){
+// 				sliderValue++;
+// 				$("#slider").val(sliderValue);
+// 				$('#range').html(sliderValue);
+// 			}
+// 			$("#slider").val(sliderValue);
+// 			updateStations();
+// 		}, duration);
+// 		running = true;
+// 	}
+// });
+
+// $("#slider").on("change", function(){
+// 	updateStations();
+// 	$("#range").html($("#slider").val());
+// 	clearInterval(timer);
+// 	$("button").html("Play");
+// });
